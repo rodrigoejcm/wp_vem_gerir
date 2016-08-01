c041eb3fb519bebfaa55b20fa945097feee55bf2
Rodrigo <rodrigo.ejcm@gmail.com>
2016-08-01 18:23:08 -0300

    ajuste tela de planos

--- wp-content/themes/tema-vemgerir/page-empreendendo.php
+++ wp-content/themes/tema-vemgerir/page-empreendendo.php
@@ -17,29 +17,56 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			</br>
+			<?php
+				if ( have_posts() ) :
 
-
-			<div>RODRIGO</div>
+					if ( is_home() && ! is_front_page() ) : ?>
+						<header>
+							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						</header>
 
 			<?php
+			endif;
+
+			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
 
-				// If comments are open or we have at least one comment, load up the comment template.
+			endwhile;
 
-				// [RODRIGO] Carrega os comentarios
-				
-				/*if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;*/
+			the_posts_navigation();
 
-			endwhile; // End of the loop.
-			?>
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+	<?php
+
+	$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+
+	<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+	<ul>
+	    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+	        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+	    <?php endwhile; ?>
+	</ul>
+	    <?php wp_reset_postdata(); ?>
+	<?php else : ?>
+	    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+
 
 <?php
 // [RODRIGO] Carrega sidebar
--- wp-content/themes/tema-vemgerir/page-nossos-planos.php
+++ wp-content/themes/tema-vemgerir/page-nossos-planos.php
@@ -18,160 +18,185 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="site-main-planos">
+				<div class="row">
+					<div class="tabela-planos verde-a col-md-4">
+						<table class="table-striped ">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 1</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 4</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>	
+					</div>
 
-				<div class="tabela-planos verde-a">
-					<table class="table-striped ">
-						<thead>
-							<tr>
-								<th>
-									<div class="nome-plano">PLANO 1</div>
-									<hr class="hr-table">
-									<div class="valor-plano">R$ 100,00</div>
-									<div class="peridiocidade-plano">( por mŝs )</div>
-								</th>
-							</tr>	
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 1</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 2</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="cinza icone-propriedade">
-										<i class="fa fa-times fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 3</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="cinza icone-propriedade">
-										<i class="fa fa-times fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 4</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<a class="botao-tabela" href="#">
-						Assinar 
-					</a>	
+					<div class="tabela-planos verde-b col-md-4">	
+						<table class="table-striped">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 2</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 4</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>
+					</div>
+					<div class="tabela-planos verde-c col-md-4">
+						<table class="table-striped">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 3</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>
+					</div>
 				</div>
-				<div class="tabela-planos verde-b">	
-					<table class="table-striped">
-						<thead>
-							<tr>
-								<th>
-									<div class="nome-plano">PLANO 2</div>
-									<hr class="hr-table">
-									<div class="valor-plano">R$ 100,00</div>
-									<div class="peridiocidade-plano">( por mŝs )</div>
-								</th>
-							</tr>	
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 1</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 2</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 3</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="cinza icone-propriedade">
-										<i class="fa fa-times fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 4</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<a class="botao-tabela" href="#">
-						Assinar 
-					</a>
+					<div class="row">
+						
+						<div class="extra-planos col-md-12">
+							<h2 class="extra-planos-header">Loren ipsum</h2>
+							
+						</div>
+					</div>
+				<div class="row">
+					<div class="texto-disclaimer col-md-12">
+						<p>
+							On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains	
+						</p>
+					</div>
 				</div>
-				<div class="tabela-planos verde-c">
-					<table class="table-striped">
-						<thead>
-							<tr>
-								<th>
-									<div class="nome-plano">PLANO 3</div>
-									<hr class="hr-table">
-									<div class="valor-plano">R$ 100,00</div>
-									<div class="peridiocidade-plano">( por mŝs )</div>
-								</th>
-							</tr>	
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 1</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 2</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 3</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="icone-propriedade">
-										<i class="fa fa-check fa-2x" aria-hidden="true"></i>
-									</div>
-									<div class="descricao-propriedade">Propreidade 1</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<a class="botao-tabela" href="#">
-						Assinar 
-					</a>
+				<div class="row">
+					<div class="texto-disclaimer col-md-12">
+						<p>
+							On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains	
+						</p>
+					</div>
 				</div>
+				</div>
+
 			</div>
 
 		</main><!-- #main -->
