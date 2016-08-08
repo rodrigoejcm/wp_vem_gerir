f74843fa14864394c4bfed166a6cf0ce58093f1b
Rodrigo <rodrigo.ejcm@gmail.com>
2016-08-07 22:07:56 -0300

    mudancas pagina inicial

--- wp-content/themes/tema-vemgerir/index.php
+++ wp-content/themes/tema-vemgerir/index.php
@@ -17,46 +17,94 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		
-		<div class="site-main-intro col-md-12">
-			<h1>passo a passo</h1>	
+		<!-- ROW 1 -->
+		
+		<div class="row">
+			<div class="col-md-12">
+				<p class="texto">
+					Nós, como empreendedores que somos, sabemos dos principais desafios, das lutas de todos os dias e da dificuldade de ver o seu resultado no final do mês. Nós nos identificamos com você e criamos soluções personalizadas pra cada tipo de negócio.
+				</p>
+			</div>
+		</div>	
+
+		<!-- ROW 2 -->
+
+		<div class="row">
+			<div class="col-md-6">
+					<div class="info-box">
+						<div class="centralizar">
+							<p class="header header-box">Vem</p>
+							<p class="header header-box">criar!</p>	
+							<p class="texto texto-box">
+								Se você quer iniciar seu próprio negócio, a Vem Gerir te dá o suporte
+								necessário para você ver que empreender é simples!
+							</p>
+						</div>
+					</div>
+			</div>
+
+			<div class=" col-md-6">
+				<div class="info-box">
+						<div class="centralizar">
+							<p class="header header-box">Vem</p>
+							<p class="header header-box">crescer!</p>	
+							<p class="texto texto-box">
+								Se você tem uma empresa,organizamos seu negócio passo a passo!
+							</p>
+						</div>
+				</div>
+			</div>
 		</div>
-		
-		<h1>É Simples.</h2>
-		
-		
-			
-		<div class="hr-div">
+
+		<!-- ROW 3 -->
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1>É simples.<h1/>	
+			</div>
+		</div>
+
+		<!-- ROW 4 -->
+
+
+		<div class=" hr-div">
 			<span>Clientes</span>
 			<hr>
 		</div>
 
+		
+		<!-- ROW 5 -->
+
 		<div class="site-main-clientes row">
 			<div class="site-main-clientes-descricao col-md-6">
-				<div class="placeholder-descricao">
+				<p class="texto">
 					Você tem o seu próprio negócio, trabalha 24 horas por dias e mesmo assim não consegue ver a cor do dinheiro? Uma rotina de trabalho pesada, contas a pagar, dinheiro pra receber, funcionários e fornecedores pra controlar, atendimento ao cliente... Difícil gerenciar tudo isso sozinho, não é? Foi pensando em você que criamos nossas soluções para simplificar a sua vida. Vem gerir com a gente!
-				</div>
+				</p>
 			</div>
 			<div class="site-main-clientes-depoimentos col-md-6">
-				<div class="placeholder-depoimentos">
-					
-				</div>
+				
+				<div class="testimonial-quote group">
+			        <img src="http://placehold.it/120x120">
+			        <div class="quote-container">
+			            <blockquote>
+			                <p>	Seu Renato, após vender
+								pastéis por cinco anos sem ver a
+								cor do dinheiro, viu sua vendinha
+								crescer após contratar a Vem
+								Gerir. Sabe certinho quanto ainda
+								tem pra gastar até o final do mês”</p>
+			            </blockquote>  
+			            <cite><span>Seu Renato</span><br>
+			                Livreiro
+			            </cite>
+			        </div>
+			    </div>
+
 			</div>
 		</div>
 
+		<!-- ROW 6 -->
 		
-		<hr class="hr-div-esp">
-
-		<div class="hr-div">
-			<span>Consultoria Individual</span>
-			<hr>
-		</div>
-		<div class="site-main-consultoria row">
-			<div class="site-main-consultoria-descricao col-md-12">
-				<div class="placeholder-consultoria">
-				</div>
-			</div>
-		</div>
-
 		<hr class="hr-div-esp">
 
 		<div class="hr-div">
@@ -66,9 +114,18 @@ get_header(); ?>
 
 		<div class="site-main-sistema row">
 			<div class="site-main-sistema-descricao col-md-4">
-				<div class="placeholder-sistema-descricao">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</div>			
+				<p class="texto">
+					A Vem Gerir é muito mais do
+					que uma ferramenta para
+					controlar o seu dinheiro.
+				</p>
+				<p class="texto">
+					É uma plataforma que te
+					ajuda a criar uma rotina
+					gerencial, a controlar suas
+					finanças e suas principais
+					tarefas
+				</p>			
 			</div>
 			<div class="site-main-sistema-imagem col-md-8">
 				<div class="placeholder-sistema-imagem">
---
+++ wp-content/themes/tema-vemgerir/page-vem-criar.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package tema-vemgerir
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div class="site-main-planos">
+				<div class="row">
+					<div class="tabela-planos verde-a col-md-4">
+						<table class="table-striped ">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 1</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 4</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>	
+					</div>
+
+					<div class="tabela-planos verde-b col-md-4">	
+						<table class="table-striped">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 2</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="cinza icone-propriedade">
+											<i class="fa fa-times fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 4</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>
+					</div>
+					<div class="tabela-planos verde-c col-md-4">
+						<table class="table-striped">
+							<thead>
+								<tr>
+									<th>
+										<div class="nome-plano">Plano 3</div>
+										<hr class="hr-table">
+										<div class="valor-plano">R$ 00,00</div>
+										<div class="peridiocidade-plano">( por mŝs )</div>
+									</th>
+								</tr>	
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 2</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 3</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="icone-propriedade">
+											<i class="fa fa-check fa-2x" aria-hidden="true"></i>
+										</div>
+										<div class="descricao-propriedade">Propreidade 1</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<a class="botao-tabela" href="#">
+							Assinar 
+						</a>
+					</div>
+				</div>
+					<div class="row">
+						
+						<div class="extra-planos col-md-12">
+							<h2 class="extra-planos-header">Loren ipsum</h2>
+							
+						</div>
+					</div>
+				<div class="row">
+					<div class="texto-disclaimer col-md-12">
+						<p>
+							On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains	
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="texto-disclaimer col-md-12">
+						<p>
+							On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains	
+						</p>
+					</div>
+				</div>
+				</div>
+
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// [RODRIGO] Carrega sidebar
+// posts e comentarios recentes, metas e categorias
+//get_sidebar();
+get_footer();
